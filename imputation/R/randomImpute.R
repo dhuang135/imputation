d11ff454fa4bc6247222c9be10a46283b38b7cff
Jonathan Min <jonathanheromin@gmail.com>
2023-03-16 13:38:54 -0700

    Updated randomImpute and regImpute

--- imputation/R/randomImpute.R
+++ imputation/R/randomImpute.R
@@ -1,5 +1,6 @@
 # Function called randomImpute
-#
+# Last Updated: 3/16/23
+
 
 # Some useful keyboard shortcuts for package authoring:
 #
@@ -11,7 +12,7 @@ randomImpute<- function(data){
   ### Naive random imputation ###
   numCols <- dim(data)[2]
   for (i in 1:numCols){
-    col <- missingCars[,i]
+    col <- data[,i]
     colNA <- col %>% is.na()
     col_numNA <- sum(colNA)
     col_imputed_values <- sample(col[!colNA] %>% unique, size=col_numNA, replace=TRUE)
